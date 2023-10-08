b37edba9d62d5bc7b35b1fd152723e8a2f9e1eae
franbusleiman <franbusleiman@gmail.com>
2023-10-08 11:55:01 -0300

    Initial commit

---
+++ src/main/java/com/busleiman/qbank/QBankApplication.java
@@ -0,0 +1,13 @@
+package com.busleiman.qbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QBankApplication.class, args);
+	}
+
+}
